1a8e9b4c1328cfd873e1afb56528cc7d9d336aac
streeg <guilhermeandreuce@gmail.com>
2017-11-26 12:09:44 -0200

    adicionado novos .sql

---
+++ anexos/seed_atualizado.sql
@@ -0,0 +1,35 @@
+insert into bd_enem_2015.CorProva values(0, "Azul");
+insert into bd_enem_2015.CorProva values(1, "Amarelo");
+insert into bd_enem_2015.CorProva values(2, "Branco");
+insert into bd_enem_2015.CorProva values(3, "Rosa");
+insert into bd_enem_2015.CorProva values(4, "Branco - Adaptada(Braille)");
+insert into bd_enem_2015.CorProva values(5, "Azul - Reaplicação");
+insert into bd_enem_2015.CorProva values(6, "Amarelo - Reaplicação");
+insert into bd_enem_2015.CorProva values(7, "Branco - Reaplicação");
+insert into bd_enem_2015.CorProva values(8, "Rosa - Reaplicação");
+insert into bd_enem_2015.CorProva values(9, "Cinza");
+insert into bd_enem_2015.CorProva values(10, "Cinza - Adaptada(Braille)");
+insert into bd_enem_2015.CorProva values(11, "Cinza - Reaplicação");
+
+insert into bd_enem_2015.AreaConhecimento values(0, "CN", "Ciências da Natureza");
+insert into bd_enem_2015.AreaConhecimento values(1, "CH", "Ciências Humanas");
+insert into bd_enem_2015.AreaConhecimento values(2, "LC", "Linguagens e códigos");
+insert into bd_enem_2015.AreaConhecimento values(3, "MT", "Matemática");
+
+
+insert into bd_enem_2015.TipoProva values(231, "AEAEADECEADEDBDACBBCEEECBADCEBCEACBBBDADCCABD",1,0);
+insert into bd_enem_2015.TipoProva values(232, "DADCCABDEACBCEEDECBABBCEBCDECBBEADBDAAEAEADEC",1,1);
+insert into bd_enem_2015.TipoProva values(233, "DADCCABDEACBCEEDECBABBCEBCDECBBEADBDAAEAEADEC",1,2);
+insert into bd_enem_2015.TipoProva values(234, "ADECAEAEDBDADEEACEBCECBACBBCEEBBDEACBCABDDADC",1,3);
+insert into bd_enem_2015.TipoProva values(235, "DCCDBCAAEBECBAADADBDEDEEBBBCABCCEAAEEDCBDDCEA",0,0);
+insert into bd_enem_2015.TipoProva values(236, "AEDCADBCABCCBBBBEEDCEAABECBDDCCDEAEEDEDAADBCA",0,1);
+insert into bd_enem_2015.TipoProva values(237, "EADDCCBEEDCEAABBBEEADBCABCDEDAADBECBAEBCACDDC",0,2);
+insert into bd_enem_2015.TipoProva values(238, "DDCEEEABBBAADDEDBECBCABCAEDCBCAADBCEAAEEDCBCD",0,3);
+insert into bd_enem_2015.TipoProva values(239, ",CDEAADCDBABDEBECECCDBBEBEABBABDCDADEEDCDAECCABBDCE",2,1);
+insert into bd_enem_2015.TipoProva values(240, "AACDEBACDDBEDDCEABBAECCDCDDEEEABBABDCDABEBDBCECCBE",2,9);
+insert into bd_enem_2015.TipoProva values(241, "DEAACDCBDAEBDABBBEDEEBABDCDADBBEBDCDAECCEABDCECECC",2,0);
+insert into bd_enem_2015.TipoProva values(242, "CEDAABDACDBDEDBDCDDCEDCDABEBAECCEABCECCBABDEEBEABB",2,3);
+insert into bd_enem_2015.TipoProva values(243, "CEDAABDACDBDEDBDCDDCEDCDABEBAECCEABCECCBABDEEBEABB",3,1);
+insert into bd_enem_2015.TipoProva values(244, "EBCCBEEDABDBDECDACCEDCCECAAEADCBEDAEACABADDBB",3,9);
+insert into bd_enem_2015.TipoProva values(245, "BADEBCCDACCCACAAECEADCBEDAEAABDEDCBDECDBBBEED",3,0);
+insert into bd_enem_2015.TipoProva values(246, "DBBBADCADAEAEDCADCBECAAEDACCCEBDECABDBEEDEBCC",3,3);
